f05059714eb889f9ce8c938233db785810d27672
Elkin Garcia <elkingarcia@Elkins-MBP.home>
2023-02-05 23:41:27 -0500

    rates page connected to firebase db

--- src/pages/RatesPage.vue
+++ src/pages/RatesPage.vue
@@ -1,108 +1,92 @@
 <template>
-  <q-list>
-    <q-expansion-item expanded-icon="arrow_drop_down" expand-icon="arrow_right">
-      <template v-slot:header>
-        <q-item-section avatar>
-          <q-avatar size="70px">
-            <img src="../assets/box.png" />
-          </q-avatar>
-        </q-item-section>
+  <q-scroll-area style="height: 85vh">
+    <q-list>
+      <q-expansion-item
+        v-for="(category, index) in ratesList"
+        :key="category.name_en"
+        expanded-icon="arrow_drop_down"
+        expand-icon="arrow_right"
+      >
+        <template v-slot:header>
+          <q-item-section avatar>
+            <q-avatar size="70px">
+              <img :src="`../assets/${category.name_en}.png`" />
+            </q-avatar>
+          </q-item-section>
 
-        <q-item-section class="text-h5"> Category </q-item-section>
-      </template>
-      <div class="rates_row">
-        <q-item-section class="text-subtitle1 q-pl-lg q-py-sm">
-          Item description description description description description
-          description description description description description
-        </q-item-section>
-        <q-item-section avatar>
-          <q-avatar size="70px" font-size="20px" text-color="white">
-            $450+
-          </q-avatar>
-        </q-item-section>
-      </div>
-    </q-expansion-item>
-    <q-separator horizontal />
-    <q-expansion-item expanded-icon="arrow_drop_down" expand-icon="arrow_right">
-      <template v-slot:header>
-        <q-item-section avatar>
-          <q-avatar size="70px">
-            <img src="../assets/drum.png" />
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section class="text-h6"> Category </q-item-section>
-      </template>
-
-      <q-item class="rates_row">
-        <q-item-section class="text-subtitle1 q-pl-lg q-py-sm"
-          >Item description description description description description
-          description description description description
-          description</q-item-section
-        >
-        <q-item-section avatar>
-          <q-avatar size="70px" font-size="20px" text-color="white">
-            $450+
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-    </q-expansion-item>
-    <q-separator horizontal />
-    <q-expansion-item expanded-icon="arrow_drop_down" expand-icon="arrow_right">
-      <template v-slot:header>
-        <q-item-section avatar>
-          <q-avatar size="70px">
-            <img src="../assets/appliance.png" />
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section class="text-h6"> Category </q-item-section>
-      </template>
-
-      <q-item class="rates_row">
-        <q-item-section class="text-subtitle1 q-pl-lg q-py-sm"
-          >Item description description description description description
-          description description description description
-          description</q-item-section
-        >
-        <q-item-section avatar>
-          <q-avatar size="70px" font-size="20px" text-color="white">
-            $450+
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-    </q-expansion-item>
-    <q-separator horizontal />
-    <q-expansion-item expanded-icon="arrow_drop_down" expand-icon="arrow_right">
-      <template v-slot:header>
-        <q-item-section avatar>
-          <q-avatar size="70px">
-            <img src="../assets/furniture.png" />
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section class="text-h6"> Category </q-item-section>
-      </template>
-
-      <q-item class="rates_row">
-        <q-item-section class="text-subtitle1 q-pl-lg q-py-sm"
-          >Item description description description description description
-          description description description description
-          description</q-item-section
-        >
-        <q-item-section avatar>
-          <q-avatar size="70px" font-size="20px" text-color="white">
-            $450+
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-    </q-expansion-item>
-    <q-separator horizontal />
-  </q-list>
+          <q-item-section v-if="$i18n.locale == 'en-US'" class="text-h5">
+            {{ category.name_en }} </q-item-section
+          ><q-item-section v-else class="text-h5">
+            {{ category.name_es }}
+          </q-item-section>
+        </template>
+        <q-list bordered separator>
+          <q-item
+            clickable
+            v-for="item in listOfLists[index]"
+            :key="item.id"
+            class="q-py-md"
+          >
+            <q-item-section class="text-h6">
+              <q-item-label v-if="$i18n.locale == 'en-US'">{{
+                item.name_en
+              }}</q-item-label>
+              <q-item-label v-else>{{ item.name_es }}</q-item-label>
+            </q-item-section>
+            <q-item-section side center>
+              <q-badge
+                color="primary"
+                :label="item.price"
+                class="text-subtitle1"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-expansion-item>
+    </q-list>
+  </q-scroll-area>
 </template>
 
 <script lang="ts">
 import '../css/rates.scss';
+import db from '../boot/firebase';
+import { collection, DocumentData, getDocs } from 'firebase/firestore/lite';
 
-export default {};
+let rL: DocumentData[] = [];
+let arrayOfiL: DocumentData[][] = [];
+export default {
+  data: function () {
+    return {
+      ratesList: rL,
+      listOfLists: arrayOfiL,
+    };
+  },
+  async created() {
+    const ratesRef = collection(db, 'rates');
+    const ratesSnapshot = await getDocs(ratesRef);
+    const rL = ratesSnapshot.docs.map((doc) => doc.data());
+    this.ratesList = rL;
+
+    let itemsRef = collection(db, 'rates/0/items');
+    let itemsSnapshot = await getDocs(itemsRef);
+    let iL = itemsSnapshot.docs.map((doc) => doc.data());
+    arrayOfiL.push(iL);
+
+    itemsRef = collection(db, 'rates/1/items');
+    itemsSnapshot = await getDocs(itemsRef);
+    iL = itemsSnapshot.docs.map((doc) => doc.data());
+    arrayOfiL.push(iL);
+
+    itemsRef = collection(db, 'rates/2/items');
+    itemsSnapshot = await getDocs(itemsRef);
+    iL = itemsSnapshot.docs.map((doc) => doc.data());
+    arrayOfiL.push(iL);
+
+    itemsRef = collection(db, 'rates/3/items');
+    itemsSnapshot = await getDocs(itemsRef);
+    iL = itemsSnapshot.docs.map((doc) => doc.data());
+    arrayOfiL.push(iL);
+    this.listOfLists = arrayOfiL;
+  },
+};
 </script>
